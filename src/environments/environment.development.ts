cf6f9d43ccf5d544fe9d4c3f1ed02e5ce8abd483
gagan947 <gagan.ctinfotech@gmail.com>
2026-04-14 15:10:32 +0530

    sidebar changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
       production: false,
-      // apiUrl: 'http://192.168.1.23:4500/api/admin/',
+      apiUrl: 'http://192.168.1.33:4500/api/admin/',
       imageUrl: 'https://api.creativethoughts.ai',
-      apiUrl: 'https://api.creativethoughts.ai/api/admin/'
+      // apiUrl: 'https://api.creativethoughts.ai/api/admin/'
 
 };
